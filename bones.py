0752702d277ef3c579c0d3d7f9813c8447cfe3df
PabloT30 <38673968+PabloT30@users.noreply.github.com>
2018-11-07 09:37:29 +0100

    Add files via upload

---
+++ bones.py
@@ -0,0 +1,28 @@
+# Fractured bones detection on X-RAY images using Machine Learning
+# Technische Universität München
+# Final project for the lecture: Advanced Topics in IC Design
+# Winter Semester 2018/19 
+# Team members:
+    # Lisha
+    # Richard
+    # Pablo
+    # YU
+
+# Libraries
+
+# Importing the data
+
+# Preprocessing the data
+
+# Creating a Sequential Model and adding the layers
+
+    # Defining the model
+    # Compiling the model
+    # Training the model
+    # Validating the model
+
+# Evaluating the model
+
+# Saving the model
+
+# Predicting new inputs
